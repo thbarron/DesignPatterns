2ac741f09c5098572cfe1a47634ec7cf4c406f81
Tyree Barron <tyree.barron@gmail.com>
2017-09-05 16:03:12 -0400

    initial commit.

---
+++ DesignPatternsPractice/DesignPatterns.Observer/Abstract/ISubject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Observer.Abstract
+{
+    public interface ISubject
+    {
+        void RegisterObserver();
+
+        void RemoveObserver();
+
+        void NotifyObservers();
+
+    }
+}
---
+++ DesignPatternsPractice/DesignPatterns.Observer/Concrete/WeatherData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatterns.Observer.Abstract;
+
+namespace DesignPatterns.Observer.Concrete
+{
+    public class WeatherData : ISubject
+    {
+        private List<IObserver> observers;
+
+        public void RegisterObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            var observerFound = observers.Contains(observer);
+            
+                
+        }
+
+        public void NotifyObservers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
